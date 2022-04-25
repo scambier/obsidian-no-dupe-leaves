ef887a982db739f9906d5d0b8e847fba430009b1
Simon Cambier <simon.cambier@protonmail.com>
2022-04-25 21:34:52 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Obsidian "No Dupe Leaves" plugin
+
+> Let's say I have 2 open notes, _A_ and _B_.  
+> _A_ has a link to _B_.  
+> If I click on `[[B]]` from _A_, Obsidian will re-open _B_. Is there a way to just show the note if it's already open?
+
+Well, there it is.
+
+![](./images/nodupes.gif)
+
+**!! This plugin is a quick hack for something that should be a feature !!**
+
+It was made in 20 minutes, it's probably buggy, but it looks like it works.
+
---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { Plugin, Workspace } from 'obsidian'
+import { around } from 'monkey-around'
+
+// Adapted from https://github.com/jekyll/jekyll/blob/7c4f319442b6b7a457d35cef2fc1b7a480b30850/lib/jekyll/document.rb#L9
+const frontMatterRegex = /^---\s*\n(.*?)\n?^---\s?/ms
+
+// const ogSave = TextFileView.prototype.save
+const ogOpen = Workspace.prototype.openLinkText
+
+export default class AutoUpdatedDatePlugin extends Plugin {
+  async onload(): Promise<void> {
+    // Monkey-patch Obsidian
+    const patchOpen = around(Workspace.prototype, {
+      openLinkText(oldOpenLinkText) {
+        return function (...args) {
+          const name = args[0] + '.md'
+          let result
+          app.workspace.iterateAllLeaves(leaf => {
+            const viewState = leaf.getViewState()
+            if (
+              viewState.type === 'markdown' &&
+              viewState.state?.file?.endsWith(name)
+            ) {
+              result = app.workspace.setActiveLeaf(leaf)
+            }
+          })
+          if (!result) {
+            result = oldOpenLinkText && oldOpenLinkText.apply(this, args)
+          }
+          return result
+        }
+      },
+    })
+  }
+
+  onunload(): void {
+    Workspace.prototype.openLinkText = ogOpen
+  }
+}

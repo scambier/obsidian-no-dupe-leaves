2c38974ea46759c1568d4a346424de4e78d10546
Simon Cambier <simon.cambier@protonmail.com>
2022-04-26 07:55:08 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { Plugin, Workspace } from 'obsidian'
 import { around } from 'monkey-around'
 
-// Adapted from https://github.com/jekyll/jekyll/blob/7c4f319442b6b7a457d35cef2fc1b7a480b30850/lib/jekyll/document.rb#L9
-const frontMatterRegex = /^---\s*\n(.*?)\n?^---\s?/ms
-
 // const ogSave = TextFileView.prototype.save
 const ogOpen = Workspace.prototype.openLinkText
 

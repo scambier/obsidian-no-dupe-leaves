bdf6d30ab2b5ac0cd6df624af6fc8369990ab546
Simon Cambier <simon.cambier@protonmail.com>
2023-02-21 08:31:41 +0100

    #6 - Fixed jump to block/heading

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { OpenViewState, Plugin, Workspace } from 'obsidian'
+import { MarkdownView, OpenViewState, Plugin, Workspace } from 'obsidian'
 import { around } from 'monkey-around'
 
 let uninstallPatchOpen: () => void
@@ -28,14 +28,15 @@ export default class NoDupeLeavesPlugin extends Plugin {
           }
 
           // Make sure that the path ends with '.md'
-          const name = linktext + (linktext.endsWith('.md') ? '' : '.md')
+          const sanitizedPath = sanitizePath(linktext)
+
           let result = false
           // Check all open panes for a matching path
           app.workspace.iterateAllLeaves(leaf => {
             const viewState = leaf.getViewState()
             if (
               viewState.type === 'markdown' &&
-              viewState.state?.file?.endsWith(name)
+              viewState.state?.file?.endsWith(sanitizedPath.name)
             ) {
               // Found a corresponding pane
               app.workspace.setActiveLeaf(leaf)
@@ -53,6 +54,7 @@ export default class NoDupeLeavesPlugin extends Plugin {
                 openViewState,
               ])
           }
+          scrollToPosition(sanitizedPath)
           return result
         }
       },
@@ -62,4 +64,54 @@ export default class NoDupeLeavesPlugin extends Plugin {
   onunload(): void {
     uninstallPatchOpen()
   }
+}
+
+/**
+ * Set the cursor to the heading/block
+ * @param sanitizedPath
+ */
+function scrollToPosition(sanitizedPath: {
+  name: string
+  heading?: string
+  block?: string
+}) {
+  const cache = app.metadataCache.getCache(sanitizedPath.name)
+  const view = app.workspace.getActiveViewOfType(MarkdownView)
+
+  // Get the corresponding position for the heading/block
+  if (sanitizedPath.heading) {
+    const heading = cache.headings.find(
+      heading => heading.heading === sanitizedPath.heading
+    )
+    if (heading) {
+      view.editor.setCursor(heading.position.start.line)
+    }
+  } else if (sanitizedPath.block) {
+    const block = cache.blocks[sanitizedPath.block]
+    if (block) {
+      view.editor.setCursor(block.position.start.line)
+    }
+  }
+}
+
+function sanitizePath(path: string): {
+  name: string
+  heading?: string
+  block?: string
+} {
+  // Extract the #^block from the path
+  const blockMatch = path.match(/\^(.*)$/)
+  const block = blockMatch ? blockMatch[1] : undefined
+  // Remove the #^block
+  path = path.replace(/(\^.*)$/, '')
+
+  // Extract the #heading from the path
+  const headingMatch = path.match(/#(.*)$/)
+  const heading = headingMatch ? headingMatch[1] : undefined
+  // Remove the #heading
+  path = path.replace(/(#.*)$/, '')
+
+  // Make sure that the path ends with '.md'
+  const name = path + (path.endsWith('.md') ? '' : '.md')
+  return { name, heading, block }
 }
